18867afc6f6c25bda7612b8f53db88a8b3fe963f
arid-mangoss <sssnarr1@gmail.com>
2021-05-17 08:45:29 -0400

    started Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// while true :
+//     if *KBD > 0:
+//         set all pixels to black
+//     else: 
+//         set all pixels to white
+
+(loop)
+    @KBD
+    D=M
+
+    @black
+    D;JGT
+
+    @white
+    0;JMP
+
+    (black)
+        // make all the pixels black
+        @SCREEN
+        D=A
+
+        @blackCounter
+        M=0
+
+        (blackLoop)
+            @blackCounter
+            D=M
+
+            @SCREEN
+            A=A+D
+            M=-1
+
+            @blackCounter
+            M=M+1
+            D=M
+
+            @8192
+            D=A-D
+            @blackLoop
+            D;JGE
+
+        @loop // back to beginning
+        0;JMP
+
+
+    (white)
+        // make all the pixels white
+        @SCREEN
+        D=A
+
+        @whiteCounter
+        M=0
+
+        (whiteLoop)
+            @whiteCounter
+            D=M
+
+            @SCREEN
+            A=A+D
+            M=0
+
+            @whiteCounter
+            M=M+1
+            D=M
+
+            @8192
+            D=A-D
+            @whiteLoop
+            D;JGE
+
+        @loop // back to beginning
+        0;JMP
+
+@loop // back to beginning
+0;JMP
+
+// end of program
+(end)
+@end
+0;JMP
